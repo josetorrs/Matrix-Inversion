73abf1d0d6a5a056780cc90476157af110f2b2d8
Andrew Pensoneault <andrew-pensoneault@uiowa.edu>
2019-11-22 14:13:58 -0600

    Removed all errors from code, still untested

--- src/MI_gpu.cu
+++ src/MI_gpu.cu
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+#include <cuda_gl_interop.h>
 #include <assert.h>
 #include <sys/time.h>
 
@@ -8,81 +11,79 @@ __global__ void GaussJordan_gpu(float *Aaug, float *subpivot, int N, int iter) {
     __shared__ float smem_a[Tix*Tix];
     int c = iter + blockIdx.x * Tix*Tix;
     int r = iter + blockIdx.y;
-    float pivot, crat;
+    float crat;
     int ti = threadIdx.x;
     smem_a[ti] = Aaug[iter*2*N + c + ti];
     crat =  Aaug[r*2*N+iter];
 
     if (c + ti < 2*N){
-        A[r*2*N+c+iter+threadIdx.x] -= crat*smem_a[threadIdx.x];
+        Aaug[r*2*N+c+iter+threadIdx.x] -= crat*smem_a[threadIdx.x];
     }
     if (blockIdx.x == 0){
-        if (threadIdx.x == 0)
-            subpivot[r] = A[r*2*N+c]
+        if (threadIdx.x == 0){
+            subpivot[r] = Aaug[r*2*N+c];
+        }
         if (blockIdx.y == 0){
-            if (threadIdx.x == 0)
-                subpivot[r] = A[iter+iter*2*N]
+            if (threadIdx.x == 0){
+                subpivot[iter] = Aaug[iter+iter*2*N];
+            }
         }
     }
 }
 
-__global__ void update_row_gpu(float *Aaug, float *subpivot, int iter, int use) {
-    __shared__ float smem_a[Tix*Tix];
+__global__ void update_row_gpu(float *Aaug, float *subpivot, int N, int iter, int use) {
     int c = iter + blockIdx.x * Tix*Tix;
-    float pivot, crat;
     int ti = threadIdx.x;
     if (c + ti < 2*N){
-        A[iter*2*N+c+threadIdx.x] += A[use*2*N+c+threadIdx.x];
+        Aaug[iter*2*N+c+threadIdx.x] += Aaug[use*2*N+c+threadIdx.x];
     }
     if (blockIdx.x == 0){
-        if (threadIdx.x == 0)
-            subpivot[r] = A[iter+iter*2*N]
+        if (threadIdx.x == 0){
+            subpivot[iter] = Aaug[iter+iter*2*N];
         }
     }
 }
 
-__global__ void scale_row_gpu(float *Aaug, float *subpivot, int iter) {
-    __shared__ float smem_a[Tix*Tix];
-    int c = use + blockIdx.x * Tix*Tix;
+__global__ void scale_row_gpu(float *Aaug, float *subpivot, int N, int iter) {
+    int c = iter + blockIdx.x * Tix*Tix;
     int ti = threadIdx.x;
     if (c + ti < 2*N){
-        A[iter*2*N+threadIdx.x] += A[iter*2*N+threadIdx.x]/subpivot[iter];
+        Aaug[iter*2*N+threadIdx.x] += Aaug[iter*2*N+threadIdx.x]/subpivot[iter];
     }
 }
 
-__global__ void Backsolve_gpu(float *Aaug, float *subpivot int N, int iter) {
+__global__ void Backsolve_gpu(float *Aaug, int N, int iter) {
     __shared__ float smem_a[Tix*Tix];
     int c = N + blockIdx.x * Tix*Tix;
     int r = blockIdx.y;
-    float pivot, crat;
+    float crat;
     int ti = threadIdx.x;
     smem_a[ti] = Aaug[iter*2*N + c + ti];
     crat =  Aaug[r*2*N+iter];
-
     if (c + ti < 2*N){
-        A[r*2*N+c+iter+threadIdx.x] -= crat*smem_a[threadIdx.x];
+        Aaug[r*2*N+c+iter+threadIdx.x] -= crat*smem_a[threadIdx.x];
     }
 }
 
 
-}
+
 int main(int argc, char *argv[]){
     float *Aaug, *Aaug_cu, *subpivot, *subpivot_cu;
-    int iter, m, i;
-    FILE * f, fout;
-
-
-    int memSize = 2*N*N*sizeof(float);
+    int iter, m, i, j,  N, use;
+    FILE * f;
 
     // Checks to see valid number of inputs given 
     if (argc > 4 || argc < 4){
         printf("Error: expected 3 input (A .mtx file, Ainv .mtx file, N), given %d\n", argc-1);
         exit(-1);
     }
+    N = strtol(argv[3],NULL,10);
+
 
     // Checks to see if a valid .mtx file was given
-    n = strtol(argv[3],NULL,10);
+    int memSize = 2*N*N*sizeof(float);
     Aaug = (float *)malloc(memSize);	
+    subpivot = (float *)malloc(N);	
 
     f = fopen(argv[1], "rb");
     for (i=0; i<N; i++){
@@ -100,6 +101,7 @@ int main(int argc, char *argv[]){
     fclose(f);
 
     cudaMalloc((void**)&Aaug_cu, memSize);
+    cudaMalloc((void**)&subpivot_cu, N);
     cudaMemcpy(Aaug, Aaug_cu, memSize, cudaMemcpyHostToDevice);
     cudaMemcpy(subpivot, subpivot_cu, memSize, cudaMemcpyHostToDevice);
 
@@ -112,40 +114,42 @@ int main(int argc, char *argv[]){
     dim3 nthreads_1(32*32, 1);
 
     for (iter=0;iter<N; iter++){
-        cudaMemcpy(subpivot_cu, subpivot, memSize, cudaMemcpyDevicetoHost);
+        cudaMemcpy(subpivot_cu, subpivot, memSize, cudaMemcpyDeviceToHost);
+                                       
         if (sqrt(subpivot[iter]*subpivot[iter])<.00000000000001){      // checks for invertability
             for (m=1; m+iter<N; m++){                // loops through lower rows for nonzero in pivot
                 if (sqrt(subpivot[iter+m]*subpivot[iter+m])>.000000000000001){   // checks if nonzero pivot
                     use = m+iter;
                     goto update;                // exits if pivot found
                 }
-                else if(m==n-1){
+                else if(m==N-1){
                     printf("Error matrix not invertible \n"); // if no pivot found, not inverable
                     exit(-1);
                 }
             }
             printf("Error matrix not invertible \n");         // if at the last pivot and zero, not invertable
             exit(-1);
-            update: a = update_row_gpu<<<nblocks_1, nthreads_1>>>(Aaug_cu, subpivot_cu, N, iter, use);
+            update: update_row_gpu<<<nblocks_1, nthreads_1>>>(Aaug_cu, subpivot_cu, N, iter, use);
+            cudaDeviceSynchronize();
         }
-        rescale_rows_gpu<<<nblocks_1, nthreads_2>>>(Aaug_cu, subpivot_cu,Aaug_diag, N, iter);
-        cudaEventSynchronize()
+        scale_row_gpu<<<nblocks_1, nthreads_1>>>(Aaug_cu, subpivot_cu, N, iter);
+        cudaDeviceSynchronize();
         
-        matmul_gpu<<<nblocks, nthreads>>>(Aaug_cu, subpivot_cu, N, iter);
-        cudaEventSynchronize()
+        GaussJordan_gpu<<<nblocks, nthreads>>>(Aaug_cu, subpivot_cu, N, iter);
+        cudaDeviceSynchronize();
     }
 
     for (iter=N-1;iter>=0; iter--){
-        backsolve_gpu<<<nblocks, nthreads>>>(Aaug_cu, N, iter);
-        cudaEventSynchronize()
+        Backsolve_gpu<<<nblocks, nthreads>>>(Aaug_cu, N, iter);
+        cudaDeviceSynchronize();
     }
     cudaMemcpy(Aaug_cu, Aaug, memSize, cudaMemcpyDeviceToHost);
 
     // Writes output to file (for testing) blocking
     FILE *of2 = fopen(argv[2], "wb");
-    for (i=0; i<n; i++){
-        for(j=n; j<2*n; j++){
-            fprintf(of2, "%f ", A[N+2*N*i+j]);
+    for (i=0; i<N; i++){
+        for(j=N; j<2*N; j++){
+            fprintf(of2, "%f ", Aaug[N+2*N*i+j]);
         }
         fprintf(of2, "\n");
     }
